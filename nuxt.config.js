3b913210702bd6b3a565d5489463633801774e36
Dennis Wegereef <denniswegereef@gmail.com>
2019-06-09 21:36:37 +0200

    added dynamic routing and a small fallback

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,19 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Poppins:300,400,600,700&display=swap'
+      }
+    ]
   },
 
   /*
---
+++ pages/work/_item.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{item.title}}</h1>
+  </div>
+</template>
+
+<script>
+import workData from '~/static/work.json'
+
+export default {
+  workData,
+  head() {
+    return {
+      title: this.item.title
+    }
+  },
+  data() {
+    return {
+      id: this.$route.params.item
+    }
+  },
+  validate({ params }) {
+    if (workData.find(item => item.id === params.item)) {
+      return true
+    }
+  },
+  computed: {
+    item() {
+      return workData.find(item => item.id === this.id)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
